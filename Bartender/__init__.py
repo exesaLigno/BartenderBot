771bdce8666f320066a5a4874840ca45b56aace3
ouvt02 <ouvt02@gmail.com>
2021-03-31 01:48:40 +0300

    Receipes in separate files Cocktails dicts replaced with class Cocktail Added search engine Added some functions of class BarTender

--- Bartender/__init__.py
+++ Bartender/__init__.py
@@ -1,31 +1,98 @@
 import accessify
 import json
 import string
+import os
+from difflib import SequenceMatcher
+import Bartender.cocktail
 
-class Bartender:
+class BarTender:
+
+    latest_id = 0;
 
     def __init__(self):
-        self.receipes_dict = {}
-        self.loadReceipes("menu.json")
+        pass
 
-    def loadReceipes(self, name_file_recipes):
-        with open(name_file_recipes, "r") as file_receipes:
-            receipes = file_receipes.read()
-            self.receipes_dict = json.loads(receipes)
+    def setPath(self, path):
+        self.fakeroot_path = path
 
-    def dumpReceipes(self, name_file_recipes):
-        with open(name_file_recipes, "w") as file_receipes:
-            file_receipes.write(json.dumps(self.receipes_dict, indent = 4,
-                                            ensure_ascii = False))
+    def loadReceipes(self):
+        self.latest_id = 0
+        self.receipes_list = []
+        for filename in os.listdir(self.fakeroot_path + "/static/cocktails/"):
+            self.receipes_list.append(Bartender.cocktail.Cocktail(self.fakeroot_path + "/static/cocktails/" + filename))
+            self.latest_id += 1
+
+
+    def dumpReceipes(self):
+        for cocktail in self.receipes_list:
+            with open(self.fakeroot_path + "/static/cocktails/" + str(cocktail.id) +
+                        ".json", "w") as file_receipe:
+                self.latest_id += 1
+                file_receipe.write(json.dumps(cocktail.__dict__, indent = 4,
+                                                ensure_ascii = False))
 
 
     def getCocktailIngredients(self, cocktail_name):
-        return self.receipes_dict[cocktail_name]
+        for cocktail in self.receipes_list:
+            if cocktail.name == cocktail_name:
+                return cocktail.Ingredients
+        return None
 
 
-    def addCocktail(self, cocktail_name, receipe):
-        self.receipes_dict[cocktail_name] = receipe
-        self.dumpReceipes("menu.json")
+    @classmethod
+    def searchEngine(cls, cocktail_name, asked_coctail_name):
+        percent = 1/2*(SequenceMatcher(None, cocktail_name, asked_coctail_name).ratio() +
+            SequenceMatcher(None, cocktail_name, asked_coctail_name).ratio())
+        if  percent > 0.5:
+            return percent
+        return 0
+
+    def search(self, asked_coctail_name):
+        cocktails_list = []
+        for cocktail in self.receipes_list:
+            percent = self.searchEngine(cocktail.name, asked_coctail_name)
+            if percent > 0:
+                cocktails_list += [(percent, cocktail.name)]
+
+        cocktails_list.sort()
+        cocktails_list.reverse()
+
+        for k in range(0, len(cocktails_list)):
+            cocktails_list[k] = cocktails_list[k][1]
+
+        return cocktails_list
+
+    @classmethod
+    def getElemClassCocktail(cls, cocktail_dict):
+
+        cocktail = Bartender.cocktail.Cocktail()
+        cocktail.__dict__ = cocktail_dict
+
+        return cocktail
+
+
+    def addCocktail(self, cocktail_name, ingredients, receipe = "null", sweetness = "null",
+                    strength = "null", prettiness = "null", estime = "null"):
+
+        self.loadReceipes()
+        self.latest_id += 1
+        ingredients_list = []
+        if type(ingredients) == str:
+            ingredients = ingredients.lower()
+            ingredients_list = ingredients.split(',')
+
+        elif type(ingredients) == list:
+            ingredients_list = ingredients
+
+        else:
+            print("Unsupported type")
+
+        self.receipes_list += [self.getElemClassCocktail({"id": self.latest_id, "name": cocktail_name, "Ingredients": ingredients_list,
+                                "Receipe": receipe, "Sweetness": sweetness, "Strength": strength,
+                                "Prettiness": prettiness, "Estime": estime})]
+
+
+        self.dumpReceipes()
 
 
     def getShoplist(self, cocktail_names):
@@ -33,7 +100,6 @@ class Bartender:
         cocktail_names_list = []
 
         if type(cocktail_names) == str:
-            cocktail_names = cocktail_names.lower()
             cocktail_names_list = cocktail_names.split(',')
 
         elif type(cocktail_names) == list:
@@ -45,11 +111,12 @@ class Bartender:
         for i in range(0, len(cocktail_names_list)):
             cocktail_names_list[i] = cocktail_names_list[i].strip()
 
-
         shoplist = []
 
         for elem in cocktail_names_list:
-            shoplist += self.receipes_dict[elem]
+            for cocktail in self.receipes_list:
+                if elem == cocktail.name:
+                    shoplist += cocktail.Ingredients
 
         shoplist_set = set(shoplist)
         shoplist = list(shoplist_set)
@@ -88,9 +155,8 @@ class Bartender:
         for i in range(0, len(ingredients_list)):
             ingredients_list[i] = ingredients_list[i].strip()
 
-        for cocktail in self.receipes_dict:
-            if self.canDococktail(ingredients_list, self.receipes_dict[cocktail]):
-                cocktails_list += [cocktail]
-
+        for cocktail in self.receipes_list:
+            if self.canDococktail(ingredients_list, cocktail.Ingredients):
+                cocktails_list += [cocktail.name]
 
         return cocktails_list
--- Bartender/cocktail.py
+++ Bartender/cocktail.py
@@ -1,13 +1,20 @@
+import json
+
 class Cocktail:
 
-    def __init__(self, cocktail_file_name):
-        self.cocktail_file_name = cocktail_file_name
-        self.name
-        self.ingredients_list
-        self.receipe
-        self.sweetness
-        self.strength
-        self.prettiness
-        self.estim
+    def __init__(self, file_name = None):
+        if file_name == None:
+            pass
+        else:
+            self.cocktail_file_name = file_name
+            self.__dict__ = self.loadReceipe()
+
+    def loadReceipe(self):
+        cocktail_dict = {}
+        with open(self.cocktail_file_name, "r") as cocktail_file:
+            cocktail = cocktail_file.read()
+            cocktail_dict = json.loads(cocktail)
+        return cocktail_dict
+
 
         #something else
